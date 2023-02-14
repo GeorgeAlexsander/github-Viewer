ce6ee498cc922bf8d1d2ace2c8b4a100bb5c7452
GeorgeAlexsander <georgealexsanderflores@gmail.com>
2023-02-14 01:54:53 -0300

    Teste

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,17 @@
 import React from 'react';
 
-import {
-  BrowserRouter as Switch,
-  Route,
-  BrowserRouter,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import RepositoriesPage from './pages/RepositoriesPage';
 
 export default function Routes() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/:login/repositories" component={RepositoriesPage} />
         <Route exact path="/" component={MainPage} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
